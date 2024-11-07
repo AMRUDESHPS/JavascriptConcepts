935221fef5dce1ed028ddbc3e9b8bd84307e3722
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-11-07 17:10:20 +0530

    callBack and promt

---
+++ callBack.js
@@ -0,0 +1,36 @@
+// function giveTwoNumber(num1, num2, callback) {
+//   let sum = num1 + num2;
+//   let squareRoot = callback(sum);
+//   console.log("sum : " + sum);
+//   console.log("square root : " + squareRoot);
+// }
+
+// giveTwoNumber(10, 20, (sum) => {
+//   let squareRoot = sum * sum;
+//   return squareRoot;
+// });
+
+
+
+
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output:process.stdout
+// })
+
+// rl.question("Enter two numbers : ", (input)=>{
+//     let sum = 0;
+//     let arr = input.split(" ").map(Number);
+//     arr.map((value)=>{
+//         sum += value;
+//         console.log(sum);
+//     })
+//     rl.close();
+// })
+
+
+
+
